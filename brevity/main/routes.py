e28c3eab1230118ac43f8d102b8d94ff067880f1
nowshadjunaed <nowshadjunaed@gmail.com>
2021-11-30 00:43:37 +0600

    Add sort by date feature

--- brevity/main/routes.py
+++ brevity/main/routes.py
@@ -4,10 +4,15 @@ from brevity.models import Post
 main = Blueprint('main', '__name__')
 
 @main.route("/")
-@main.route("/home")
-def home():
+@main.route("/home/<int:type>",methods=['GET', 'POST'])
+def home(type=0):
     page = request.args.get('page', 1, type=int)      # type = int : to raise value error when someone passes anything other than int.
-    posts =  Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    #posts =  Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    print(type)
+    if type==0:
+        posts =  Post.query.paginate(page=page, per_page=5)
+    elif type==1:
+        posts =  Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template('home.html', posts = posts)
                                                     #paginate() returns a pagination object which has necessary attributes and methods.
                                                     #dir(object): returns all the attributes and methods of that object
@@ -15,4 +20,5 @@ def home():
 @main.route("/about")
 def about():
     return render_template('about.html', title = 'About')
+
 
