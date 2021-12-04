e6f4c38fc1a4da4b4574bab5f5698ae7c213196d
Omar Faruqe <riyad.omf@gmail.com>
2021-12-04 20:30:36 +0600

    Update sort feature

--- brevity/main/routes.py
+++ brevity/main/routes.py
@@ -26,7 +26,6 @@ def layout():
 def home(type=0):
     page = request.args.get('page', 1, type=int)      # type = int : to raise value error when someone passes anything other than int.
     #posts =  Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
-    print(type)
     if type==0:
         posts =  Post.query.paginate(page=page, per_page=5)
     elif type==1:
@@ -46,20 +45,28 @@ def about():
 
 
 @main.route('/search',methods=['GET', 'POST'])
-def search():
+@main.route('/search/<int:type>',methods=['GET', 'POST'])
+def search(type=0):
     page = request.args.get('page', 1, type=int) 
-    posts =  Post.query.paginate(page=page, per_page=5)
+
+    if type==0:
+        posts =  Post.query.paginate(page=page, per_page=5)
+    elif type==1:
+        posts =  Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    elif type==2:
+        posts = db.session.query(Post).join(Upvote).filter(Post.id==Upvote.post_id).group_by(Post.id).order_by(func.count(Post.id).desc()).paginate(page=page, per_page=5)
+
     form=SearchForm()
     if form.validate_on_submit():
         searched_val = form.searched.data
         if searched_val==None:
-            return render_template('home.html', posts = posts)
+            return render_template('searched_post.html', posts = posts)
         return render_template('searched_posts.html', posts = search_result(searched_val,page),form =form,form_val=form.searched.data)
 
     searched_val = request.args.get('form_val')
 
     if searched_val==None:
-            return render_template('home.html', posts = posts)
+            return render_template('searched_posts.html', posts = posts)
 
     type =  request.args.get('type')
     if type=="1":
--- brevity/posts/utils.py
+++ brevity/posts/utils.py
@@ -31,53 +31,6 @@ def FileSizeLimit(max_size_in_mb):
         
         return file_length_check
 
-
-
-# class MultiFileAllowed(object):
-
-#     def __init__(self, upload_set, message=None):
-#         self.upload_set = upload_set
-#         self.message = message
-
-#     def __call__(self, form, field):
-
-#         # FileAllowed only expects a single instance of FileStorage
-#         # if not (isinstance(field.data, FileStorage) and field.data):
-#         #     return
-
-#         # Check that all the items in field.data are FileStorage items
-#         if not (all(isinstance(item, FileStorage) for item in field.data) and field.data):
-#             return
-
-#         print("upor theke")
-#         for data in field.data:
-#             print(data)
-#             filename = data.filename.lower()
-#             print(filename)
-
-
-
-#         for data in field.data:
-#             if not data: 
-#                 return
-#             filename = data.filename.lower()
-#             print(filename)
-
-#             if isinstance(self.upload_set, Iterable):
-#                 if any(filename.endswith('.' + x) for x in self.upload_set) :
-#                     #return
-#                     print('thik acheeeeeeeeeeeeeee')
-#                     return
-
-#                 raise StopValidation(self.message or field.gettext(
-#                     'File does not have an approved extension: {extensions}'
-#                 ).format(extensions=', '.join(self.upload_set)))
-
-#             if not self.upload_set.file_allowed(field.data, filename):
-#                 raise StopValidation(self.message or field.gettext(
-#                     'File does not have an approved extension.'
-#                 ))
-        
         
 
 class MultiFileAllowed(object):
